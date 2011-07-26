9d7c20dccf1747f6fa7db278184bfa99f8fc367b
David Bates <dave@batez-consulting.com>
2011-07-26 13:11:03 -0500

    Added the rest of the validations for the Address Class

--- Ordrin/Address.php
+++ Ordrin/Address.php
@@ -12,17 +12,41 @@ class Address extends Ordrin {
         $phone;
 
     function __construct($street, $city, $zip, $street2, $state, $phone, $nick) {
-        $this->__set('street', $street);
-        $this->__set('city', $city);
+        $this->__set('street', urlencode($street));
+        $this->__set('city', urlencode($city));
         $this->__set('zip', $zip);
-        $this->__set('street2', $street2);
+        $this->__set('street2', urlencode($street2));
         $this->__set('state', $state);
         $this->__set('phone', $phone);
         $this->__set('nick', $nick);
     }
 
     function validate($element = "all") {
+        echo "DEBUG:: Address validate for $element\n";
+        if ($element == 'zip' && !preg_match('/(^\d{5}$)|(^\d{5}-\d{4}$)/', $this->__get($element))) {
+            parent::$_errors[] = 'Address - validation - zip code (invalid) (' . $this->__get($element) . ')';
+        } elseif ($element == 'phone' && !preg_match('/(^\(?(\d{3})\)?[- .]?(\d{3})[- .]?(\d{4})$)/', $this->__get($element))) {
+            parent::$_errors[] = 'Address - validation - phone number (invalid) (' . $this->__get($element) . ')';
+        } elseif ($element == 'city' && !preg_match('/[A-z.-]/', $this->__get($element))) {
+            parent::$_errors[] = 'Address - validation - city (invalid, only letters/spaces allowed) (' . $this->__get($element) . ')';
+        } elseif ($element == 'state' && !preg_match('/^([A-z]){2}$/', $this->__get($element))) {
+            parent::$_errors[] = 'Address - validation - state (invalid, only letters allowed and must be passed as two-letter abbreviation) (' . $this->__get($element) . ')';
+        } else {
+            // do ALL validation
+            if (!preg_match('/(^\d{5}$)|(^\d{5}-\d{4}$)/', $this->__get('zip'))) {
+                parent::$_errors[] = 'Address - validation - zip code (invalid) (' . $this->__get('zip') . ')';
+            } elseif (!preg_match('/(^\(?(\d{3})\)?[- .]?(\d{3})[- .]?(\d{4})$)/', $this->__get('phone'))) {
+                parent::$_errors[] = 'Address - validation - phone number (invalid) (' . $this->__get('phone') . ')';
+            } elseif (!preg_match('/[A-z.-]/', $this->__get('city'))) {
+                parent::$_errors[] = 'Address - validation - city (invalid, only letters/spaces allowed) (' . $this->__get('city') . ')';
+            } elseif (!preg_match('/^([A-z]){2}$/', $this->__get('state'))) {
+                parent::$_errors[] = 'Address - validation - state (invalid, only letters allowed and must be passed as two-letter abbreviation) (' . $this->__get('state') . ')';
+            }
+        }
+    }
 
+    function _convertForAPI() {
+        return $this->__get('zip') . '/' . $this->__get('city') . '/' . $this->__get('street');
     }
 
     function __set($name, $value) {
